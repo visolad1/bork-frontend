ba4f8207970977ec5a3e913893c78a0468f146e8
visola <visolad1@gmail.com>
2023-11-08 13:47:15 +0500

    Header, Main

--- src/components/MainSlider/index.jsx
+++ src/components/MainSlider/index.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import './MainSlider.scss';
 import Slider from 'react-slick';
 
 function MainSlider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -9,8 +12,12 @@ function MainSlider() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+    beforeChange: (current, next) => {
+      setCurrentSlide(next);
+    },
     pauseOnFocus: false,
-    autoplaySpeed: 4000,
+    pauseOnDotsHover: true,
+    autoplaySpeed: 5000,
     customPaging: (i) => (
       <p
         style={{
@@ -27,19 +34,31 @@ function MainSlider() {
       <Slider {...settings}>
         <a href='' className='main-slide-link'>
           <div className='main-slide slide1'>
-            <div className="container">
-                <h2></h2>
+            <div className='slide-content'>
+              <h2 className='slide-title'>Фен-стайлер F732</h2>
             </div>
           </div>
         </a>
-        <a href='' className='main-slide-link'>
-          <div className='main-slide slide2'></div>
+        <a href='#' className='main-slide-link'>
+          <div className='main-slide slide2'>
+            <div className='slide-content'>
+              <h2 className='slide-title'>Беспроводной моющий пылесос V810</h2>
+            </div>
+          </div>
         </a>
-        <a href='' className='main-slide-link'>
-          <div className='main-slide slide3'></div>
+        <a href='#' className='main-slide-link'>
+          <div className='main-slide slide3'>
+            <div className='slide-content'>
+              <h2 className='slide-title'>GLED-маска D800</h2>
+            </div>
+          </div>
         </a>
-        <a href='' className='main-slide-link'>
-          <div className='main-slide slide4'></div>
+        <a href='#' className='main-slide-link'>
+          <div className='main-slide slide4'>
+            <div className='slide-content'>
+              <h2 className='slide-title'>Массажная капсула AVALON</h2>
+            </div>
+          </div>
         </a>
       </Slider>
     </div>
